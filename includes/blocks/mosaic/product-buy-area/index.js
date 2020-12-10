27609ec5174c2465dca5e5e087e7e926d73114ab
Ralf Wiechers <ralf@cross-media-cloud.de>
2020-12-10 18:25:44 +0100

    add code for first block - product buy area

---
+++ includes/blocks/mosaic/product-buy-area/index.js
@@ -0,0 +1,53 @@
+/**
+ * Block Styles Example
+ *
+ * https://github.com/modularwp/gutenberg-block-styles-example
+ */
+( function () {
+	let __ = wp.i18n.__; // The __() function for internationalization.
+	let createElement = wp.element.createElement; // The wp.element.createElement() function to create elements.
+	let registerBlockType = wp.blocks.registerBlockType; // The registerBlockType() function to register blocks.
+	let ServerSideRender = wp.serverSideRender; // For displaying server rendered elements.
+
+	/**
+	 * Register block
+	 *
+	 * @param  {string}   name     Block name.
+	 * @param  {Object}   settings Block settings.
+	 * @return {?WPBlock}          Block itself, if registered successfully,
+	 *                             otherwise "undefined".
+	 */
+	registerBlockType(
+		'mosaic-for-woocommerce/product-buy-area', // Block name. Must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+		{
+			title: __( 'Buy area' ), // Block title. __() function allows for internationalization.
+			icon: 'cart', // Block icon from Dashicons. https://developer.wordpress.org/resource/dashicons/.
+			category: 'woocommerce', // Block category. Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+			keywords: [__( 'add to cart' ), __( 'buy' ), __( 'booking' )],
+
+			supports: {
+				html: true,
+			},
+
+			attributes: {},
+
+			// Defines the block within the editor.
+			edit: function ( {attributes, setAttributes} ) {
+				return [
+					createElement(
+						ServerSideRender,
+						{
+							block: 'mosaic-for-woocommerce/product-buy-area',
+							attributes: attributes,
+						}
+					),
+				];
+			},
+
+			// Defines the saved block.
+			save: function ( {attributes} ) {
+				return null;
+			},
+		}
+	);
+} )();
---
+++ includes/blocks/mosaic/product-buy-area/index.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Register client-side assets (scripts and stylesheets) for the block.
+ *
+ * @package mosaic-for-woocommerce
+ * @since 1.0.0
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ */
+function mosaic_for_woocommerce_blocks_products_buy_area_block_init() {
+
+	// Skip block registration if Gutenberg is not enabled/merged.
+	if ( ! function_exists( 'register_block_type' ) ) {
+		return;
+	}
+
+	// Get file directory
+	$dir = dirname( __FILE__ );
+
+	/*
+	 * Add main js file
+	 */
+	// Put main js file name into var
+	$index_js = 'index.js';
+
+	// Register main js file
+	wp_register_script(
+		'mosaic-for-woocommerce-blocks-products-buy-area-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+			'wp-components',
+			'wp-editor',
+			'wp-server-side-render',
+			'wp-compose',
+			'wp-data',
+			'wp-api',
+		),
+		filemtime( "$dir/$index_js" )
+	);
+
+	/*
+	 * Add editor style
+	 */
+	// Put editor styles file name into var
+	$editor_css = 'editor.css';
+
+	// Add editor styles
+	wp_register_style(
+		'mosaic-for-woocommerce-blocks-products-buy-area-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	/*
+	 * Frontend styles
+	 */
+	// Add frontend styles to var name
+	$style_css = 'style.css';
+
+	// Load frontend styles
+	wp_register_style(
+		'mosaic-for-woocommerce-blocks-products-buy-area-block',
+		plugins_url( $style_css, __FILE__ ),
+		array( 'mosaic-for-woocommerce-blocks-event-product-indicators', ),
+		filemtime( "$dir/$style_css" )
+	);
+
+	/*
+	 * Add the block type
+	 */
+	register_block_type( 'mosaic-for-woocommerce/products-buy-area', array(
+		'editor_script'   => 'mosaic-for-woocommerce-blocks-products-buy-area-block-editor',
+		'editor_style'    => 'mosaic-for-woocommerce-blocks-products-buy-area-block-editor',
+		'style'           => 'mosaic-for-woocommerce-blocks-products-buy-area-block',
+		'render_callback' => 'mosaic_for_woocommerce_blocks_products_buy_area_block_render_callback',
+		'attributes'      => array(),
+	) );
+}
+add_action( 'init', 'mosaic_for_woocommerce_blocks_products_buy_area_block_init', 100 );
+
+/**
+ * Render callback for block.
+ *
+ * @param array $attributes
+ * @param string $content
+ *
+ * @return string
+ */
+function mosaic_for_woocommerce_blocks_products_buy_area_block_render_callback( array $attributes, $content ) {
+
+	ob_start();
+	?>
+
+	<span id="buy-now"></span>
+
+	<?php
+	// Do the add to cart stuff
+	do_action( 'woocommerce_single_product_summary' );
+
+	// Return the markup
+	return ob_get_clean();
+}
