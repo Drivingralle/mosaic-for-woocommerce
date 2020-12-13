e44771f934ee2d385b8029597ad3a2061c907ab9
Ralf Wiechers <ralf@cross-media-cloud.de>
2020-12-13 13:58:30 +0100

    load single product summary block

--- includes/blocks/blocks.php
+++ includes/blocks/blocks.php
@@ -6,5 +6,8 @@
 // Block categories
 require_once 'block-categories/block-categories.php';
 
-// Product buy area
+// WooCommerce Actions
+require_once 'actions/single_product_summary/index.php';
+
+// Custom blocks
 require_once 'mosaic/product-buy-area/index.php';
