d4d23a3a8ea93a43af6b1f88f8081367f02df711
Ralf Wiechers <ralf@cross-media-cloud.de>
2020-12-13 13:21:46 +0100

    add block categories

---
+++ includes/blocks/block-categories/block-categories.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Creating a custom block category for mosaic blocks.
+ *
+ * @param array $categories List of block categories.
+ * @param WP_Post $post Post object.
+ *
+ * @return  array
+ */
+function mosaic_for_woocommerce_blocks_actions_category( array $categories, WP_Post $post ): array {
+
+	// Prepare data
+	$category_slugs = wp_list_pluck( $categories, 'slug' );
+
+	// List of categories to add
+	$block_categories = array(
+		'actions_category'   => array(
+			'title' => esc_html( sprintf( __( '%s: %s', 'mosaic-for-woocommerce' ), __( 'Mosaic', 'mosaic-for-woocommerce' ), __( 'WooCommerce Actions', 'mosaic-for-woocommerce' ) ) ),
+			'slug'  => 'mosaic-for-woocommerce-actions',
+			'icon'  => 'hammer',
+		),
+		'templates_category' => array(
+			'title' => esc_html( sprintf( __( '%s: %s', 'mosaic-for-woocommerce' ), __( 'Mosaic', 'mosaic-for-woocommerce' ), __( 'WooCommerce Templates', 'mosaic-for-woocommerce' ) ) ),
+			'slug'  => 'mosaic-for-woocommerce-templates',
+			'icon'  => 'welcome-widgets-menus',
+		),
+		'customs_category' => array(
+			'title' => esc_html( sprintf( __( '%s: %s', 'mosaic-for-woocommerce' ), __( 'Mosaic', 'mosaic-for-woocommerce' ), __( 'WooCommerce Templates', 'mosaic-for-woocommerce' ) ) ),
+			'slug'  => 'mosaic-for-woocommerce-custom',
+			'icon'  => 'admin-tools',
+		),
+	);
+
+	// Loop the categories
+	foreach ( $block_categories as $category ) {
+		// Check if adding is needed
+		if ( ! in_array( $category['slug'], $category_slugs, true ) ) {
+			$categories = array_merge(
+				$categories,
+				array(
+					array(
+						'title' => $category['title'], // Required
+						'slug'  => $category['slug'], // Required
+						'icon'  => $category['icon'], // Slug of a WordPress dashicon or custom SVG
+					),
+				)
+			);
+		}
+	}
+
+	return $categories;
+}
+add_filter( 'block_categories', 'mosaic_for_woocommerce_blocks_actions_category', 20, 2 );
--- includes/blocks/blocks.php
+++ includes/blocks/blocks.php
@@ -3,5 +3,8 @@
  * Load blocks.
  */
 
+// Block categories
+require_once 'block-categories/block-categories.php';
+
 // Product buy area
 require_once 'mosaic/product-buy-area/index.php';
